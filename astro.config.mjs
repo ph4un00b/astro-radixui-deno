759fe5bf03a676891e03c1b4584758023fcd0d62
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-05-02 18:42:16 -0600

    host all ips

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { host: true },
   integrations: [
     mdx(),
     tailwind({ config: { applyBaseStyles: false } }),
